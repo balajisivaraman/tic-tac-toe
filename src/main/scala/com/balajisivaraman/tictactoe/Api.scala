d50364a1203a948a0fc070904f5371124ccd9313
Balaji Sivaraman <balaji@balajisivaraman.com>
2016-05-17 00:41:04 +0530

    Initial complete implementation of move

--- src/main/scala/com/balajisivaraman/tictactoe/Api.scala
+++ src/main/scala/com/balajisivaraman/tictactoe/Api.scala
@@ -1,10 +1,34 @@
 package com.balajisivaraman.tictactoe
 
+import Position.winningPositions
+
 object Api {
 
   def move(player: Player, board: IsMovable, position: Position): Response = {
-    def successfulMove(player: Player, board: Board, position: Position): SuccessfulMove = ???
-    playerAt(board, position).map(_ => FailedMove).getOrElse(successfulMove(player, board, position))
+
+    def isEmpty(square: Square): Boolean = square match {
+      case Empty(_) => true
+      case _        => false
+    }
+
+    def isOccupiedBy(square: Square, player: Player): Boolean = square match {
+      case OccupiedBy(_, p: Player) if p == player => true
+      case _                                       => false
+    }
+
+    val successfulMove: SuccessfulMove = {
+      val movedBoard = InProgress(board.squares.map(square => if (square.position == position) OccupiedBy(position, player) else square))
+      val hasPlayerWon = winningPositions.exists(winningPosition => movedBoard.squares.filter(square => winningPosition.contains(square.position)).forall(isOccupiedBy(_, player)))
+      val newBoard =
+        if(hasPlayerWon) {
+          GameOver(movedBoard.squares)
+        } else if (movedBoard.squares.forall(!isEmpty(_))) {
+          NoMoreMoves(movedBoard.squares.map{case square: OccupiedBy => square})
+        } else movedBoard
+      SuccessfulMove(newBoard)
+    }
+
+    playerAt(board, position).map(_ => FailedMove).getOrElse(successfulMove)
   }
 
   def whoWon(finishedBoard: Finished): Option[Player] = ???
--- src/main/scala/com/balajisivaraman/tictactoe/Position.scala
+++ src/main/scala/com/balajisivaraman/tictactoe/Position.scala
@@ -5,7 +5,18 @@ case class Position(x: Int, y: Int)
 object Position {
   val getStartingPositions: List[Position] =
     for {
-      r <- List.range(0, 2)
-      c <- List.range(0, 2)
+      r <- List.range(0, 3)
+      c <- List.range(0, 3)
     } yield Position(r, c)
+  val winningPositions: List[List[Position]] =
+    List(
+      List(Position(0,0), Position(0,1), Position(0,2)),
+      List(Position(1,0), Position(1,1), Position(1,2)),
+      List(Position(2,0), Position(2,1), Position(2,2)),
+      List(Position(0,0), Position(1,0), Position(2,0)),
+      List(Position(0,1), Position(1,1), Position(2,1)),
+      List(Position(0,2), Position(1,2), Position(2,2)),
+      List(Position(0,0), Position(1,1), Position(2,2)),
+      List(Position(0,2), Position(1,1), Position(2,0))
+    )
 }
